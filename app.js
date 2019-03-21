11e0f4da9a5a8cadc58913f02b450e3c36a395da
Lucas Grellier <Lucas.Grellier@grenoble-inp.org>
2019-03-21 22:02:59 +0100

    added first landing page website

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '/public/views'));
-app.engine('mustache', mustacheExpress());
-app.set('view engine', 'mustache');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,7 +22,7 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
