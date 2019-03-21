fdfeeeaa600e48e7ce69c4044073f21b5d99032f
LaTortueSolitaire <perfor971@gmail.com>
2019-03-21 21:19:41 +0000

    change the route to catch server/enryBox url

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/enryBox', indexRouter);
+app.use('/enryBox/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
