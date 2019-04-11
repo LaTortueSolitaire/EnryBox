6cd7df102ff78d93c86679ab0604e7d00e53778f
LaTortueSolitaire <perfor971@gmail.com>
2019-04-11 12:36:44 +0200

    minor change for routes

--- routes/showChatPrivacy.js
+++ routes/showChatPrivacy.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var express = require('express');
+var router = express.Router();
 
 /* GET privacy page policy */
 router.get('/', function(req, res, next) {
